f6e266a41c821a82e2c537925c72a59ee7afe92d
Тима <TimofeyKolotovkin2@gmail.com>
2025-07-12 22:02:55 +0300

    feat: backend/Tima/domain-layer: implementation of expenseConsumer entity

--- apps/backend-app/src/main/java/com/zdk/podelim/internal/domain/entities/ExpenseConsumer.java
+++ apps/backend-app/src/main/java/com/zdk/podelim/internal/domain/entities/ExpenseConsumer.java
@@ -1,3 +1,39 @@
 package com.zdk.podelim.internal.domain.entities;
 
-public class ExpenseConsumer {}
+import java.util.Objects;
+
+public class ExpenseConsumer {
+    private long participantId;
+    private long expenseId;
+
+    public ExpenseConsumer(long participantId, long expenseId) {
+        Objects.requireNonNull(participantId);
+        Objects.requireNonNull(expenseId);
+        
+        this.participantId = participantId;
+        this.expenseId = expenseId;
+    }
+
+    public long getParticipantId() {
+        return participantId;
+    }
+
+    public void setParticipantId(long participantId) {
+        this.participantId = participantId;
+    }
+
+    public long getExpenseId() {
+        return expenseId;
+    }
+
+    public void setExpenseId(long expenseId) {
+        this.expenseId = expenseId;
+    }
+
+    @Override
+    public String toString() {
+        return "ExpenseConsumer [participantId=" + participantId + ", expenseId=" + expenseId + "]";
+    }
+
+    
+}
