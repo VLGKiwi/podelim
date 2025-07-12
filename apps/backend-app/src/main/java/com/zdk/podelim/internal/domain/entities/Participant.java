b8851e6e558c0b99272af34931e892cb3fae4a10
Тима <TimofeyKolotovkin2@gmail.com>
2025-07-12 22:03:06 +0300

    feat: backend/Tima/domain-layer: implementation of participant entity

--- apps/backend-app/src/main/java/com/zdk/podelim/internal/domain/entities/Participant.java
+++ apps/backend-app/src/main/java/com/zdk/podelim/internal/domain/entities/Participant.java
@@ -1,3 +1,66 @@
 package com.zdk.podelim.internal.domain.entities;
 
-public class Participant {}
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * domain entity participant
+ */
+public class Participant {
+    private final UUID eventId;
+    private final long id;
+    private String name;
+    /**
+     * userId field can be null if user is temporary
+     */
+    private final UUID userId;
+
+    public Participant(UUID eventId, int id, String name, UUID userId) {
+        Objects.requireNonNull(eventId);
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(name);
+        
+        this.eventId = eventId;
+        this.id = id;
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public UUID getEventId() {
+        return eventId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    @Override
+    public String toString() {
+        return "Participant [eventId=" + eventId + ", id=" + id + ", name=" + name + ", userId=" + userId + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Participant participant = (Participant) obj;
+        return Objects.equals(id, participant.id);
+    }
+}
