67e01c7f3242925435690ee4dc25fc4c705d657a
suplzzz <dualspeed34@gmail.com>
2025-07-14 19:22:33 +0300

    feat mobile/Ilya/setup data layer: done

---
+++ apps/mobile-app/app/src/main/java/com/zdk/podelim/di/AppModule.kt
@@ -0,0 +1,41 @@
+package com.zdk.podelim.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.zdk.podelim.data.remote.ApiService
+import com.zdk.podelim.data.repository.EventRepository
+import com.zdk.podelim.data.repository.MockEventRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://mock.api.podelim.com/")
+            .addConverterFactory(Json.asConverterFactory("application/json; charset=UTF8".toMediaType()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit) : ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideEventRepository(apiService: ApiService) : EventRepository {
+        return MockEventRepository()
+
+        //return EventRepositoryImpl(apiService)
+    }
+}
