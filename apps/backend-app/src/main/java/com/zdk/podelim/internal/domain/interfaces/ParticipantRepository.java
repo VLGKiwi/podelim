9f8b4f92c784f51af064caf482f66d17705be55d
Тима <TimofeyKolotovkin2@gmail.com>
2025-07-21 23:15:39 +0300

    feat: backend/Tima/domain-layer: added repos interfaces

--- apps/backend-app/src/main/java/com/zdk/podelim/internal/domain/interfaces/ParticipantRepository.java
+++ apps/backend-app/src/main/java/com/zdk/podelim/internal/domain/interfaces/ParticipantRepository.java
@@ -1,3 +1,14 @@
 package com.zdk.podelim.internal.domain.interfaces;
 
-public interface ParticipantRepository {}
+import java.util.List;
+import java.util.UUID;
+
+import com.zdk.podelim.internal.domain.entities.Participant;
+
+public interface ParticipantRepository {
+    public void save(Participant participant);
+    public List<Participant> findAllByEventId(UUID eventId);
+    public Participant findById(long participantId);
+    public void updateById(Participant participant, long participantId);
+    public void deleteById(long participantId);
+}
